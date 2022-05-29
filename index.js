69da65431b69331736ea06d42f884b73efe8246f
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-05-29 18:05:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+// const express = require('express')
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import dotenv from "dotenv";
+import { ObjectId } from 'mongodb';
+
+const app = express();
+dotenv.config();
+// const MONGO_URL = 'mongodb://localhost';
+const MONGO_URL = process.env.MONGO_URL;
+
+const PORT = 5000;
+
+async function createConnection() {
+    const client = new MongoClient(MONGO_URL)
+    await client.connect();
+    console.log("MongoDB is connected ✅")
+    return client;
+}
+const client = await createConnection();
+
+const [users, setUsers] = [
+    {
+        fname: "omkar",
+        lname: "gavhane",
+        email: "ogomkargavhane@gmail.com",
+        password: "let@123",
+        username: "omkar123",
+        contact: 9191919191
+    }
+]
+
+app.use(express.json()); // converts data to json
+// third party package - middleware
+// app.use(cors());   //anybody can access this data from API
+
+// USERS
+app.get('/', function (req, res) {
+    res.send('Hello User, Hope you like our services')
+})
+app.get('/users', async function (req, res) {
+    const data = await client.db('crm').collection('users').find({}).toArray();
+    res.send(data);
+})
+app.get('/users/:username', async function (req, res) {
+    const param = req.params;
+    const querydata = await client.db('crm').collection('users').find({ username: param.username }).toArray();
+    res.send(querydata);
+})
+app.post('/users', async function (req, res) {
+    const newUser = req.body;
+    const data = await client.db('crm').collection('users').insertOne(newUser);
+    res.send(data);
+})
+
+// LEADS
+app.get('/leads', async function (req, res) {
+    const data = await client.db('crm').collection('leads').find({}).toArray();
+    res.send(data);
+})
+app.get('/leads/:id', async function (req, res) {
+    const param = req.params;
+    const querydata = await client.db('crm').collection('leads').find({ _id: ObjectId(param.id) }).toArray();
+    res.send(querydata);
+    // console.log(param.id);
+})
+app.post('/leads', async function (req, res) {
+    const newLead = req.body;
+    const data = await client.db('crm').collection('leads').insertOne(newLead);
+    res.send(data);
+})
+
+
+// SERVICES
+app.get('/services', async function (req, res) {
+    const data = await client.db('crm').collection('services').find({}).toArray();
+    res.send(data);
+})
+app.get('/services/:id', async function (req, res) {
+    const services = req.params;
+    const querydata = await client.db('crm').collection('services').find({ _id: ObjectId(services.id) }).toArray();
+    res.send(querydata);
+    // console.log(param.id);
+})
+app.post('/services', async function (req, res) {
+    const newLead = req.body;
+    const data = await client.db('crm').collection('services').insertOne(newLead);
+    res.send(data);
+})
+
+app.listen(PORT, () => console.log(`Started server at ${PORT} 😎`));
