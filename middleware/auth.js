e0447e0aa02205e137e467084b48136b79cad9df
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-06-02 21:17:39 +0530

    app was not working so updated nothing and uploading again

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const auth = (req, res, next) => {
     try {
         const token = req.header("x-auth-token");
-        // console.log(token);
+        // console.log(token); //
         jwt.verify(token, process.env.SECRET_KEY)
         next();
     } catch (err) {
