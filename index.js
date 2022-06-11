9098b251811a4fdf189c696d8ec20023e42452b8
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-06-11 12:55:23 +0530

    minor semi-colon chnges

--- index.js
+++ index.js
@@ -83,13 +83,13 @@ app.post('/users/login', async function (req, res) {
     const { username, password } = req.body;
     const checkUsername = await client.db('crm').collection('users').findOne({ username: username });
     if (!checkUsername) {
-        res.send({ "error": "invalid credentials - user" });
+        res.send({ "error": "invalid credentials - user" })
         alert("invalid credentials - user")
     } else {
         // const storedPassword = checkUsername.password;
         // const isPasswordMatch = await bcrypt.compare(password, storedPassword);
 
-        const isPasswordMatch = (password == checkUsername.password);
+        const isPasswordMatch = (password == checkUsername.password)
 
         if (isPasswordMatch) {
             // const token = jwt.sign({ id: checkUsername._id }, process.env.SECRET_KEY);
