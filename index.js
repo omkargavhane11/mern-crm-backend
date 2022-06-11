72328f7422a1e6b41321140f3cf5b47da280228a
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-06-11 21:20:58 +0530

    jwt implemented

--- index.js
+++ index.js
@@ -90,8 +90,8 @@ app.post('/users/login', async function (req, res) {
         const isPasswordMatch = (password == checkUsername.password)
 
         if (isPasswordMatch) {
-            // const token = jwt.sign({ id: checkUsername._id }, process.env.SECRET_KEY);
-            res.send(checkUsername)
+            const token = jwt.sign({ id: checkUsername._id, name: checkUsername.fname, email: checkUsername.email }, process.env.SECRET_KEY);
+            res.send({ token: token, data: checkUsername })
         } else {
             res.send(false)
         }
