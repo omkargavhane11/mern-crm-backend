b01e477263fc965f3fb85e9390be9bd6602e17af
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-06-12 08:37:21 +0530

    login data fetch updated

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/login', async function (req, res) {
 
         if (isPasswordMatch) {
             const token = jwt.sign({ id: checkUsername._id }, 'some123');
-            res.send({ token: token })
+            res.send({ fname: checkUsername.fname, username: checkUsername.username, email: checkUsername.email, token: token })
         } else {
             res.send(false)
         }
